7e436bcffa47ea4ab03602cbf7a4e505fca3455b
Md Rizwan Ansari <85256728+mdrizwan7739@users.noreply.github.com>
2023-02-05 19:43:45 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
+import Info from './Info';
+import Blog from './Blog';
 const Home = () => {
+    const navigate=useNavigate();
+    const handleInfo=()=>{
+       navigate("/Info");
+    }
+    const handleBlog=()=>{
+       navigate("/Blog");
+    }
 
     return (
         <div id='home-page'>
@@ -8,8 +17,8 @@ const Home = () => {
             <h1>Home page</h1>
             <h2>Hi, my name is Siddharth </h2>
             <h2>and I work at <a href="https://www.newtonschool.co/">Newton School</a></h2>
-           <button id="home-to-blog"> Go to Blog</button>
-            <button id="home-to-info"> Go to Info</button>
+           <button id="home-to-blog"  onClick={handleBlog}> Go to Blog</button>
+            <button id="home-to-info"  onClick={handleInfo}> Go to Info</button>
 
         </div>
     )
