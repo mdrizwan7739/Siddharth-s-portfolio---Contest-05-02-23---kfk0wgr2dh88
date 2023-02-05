a99ae541ac66a5d5b9868bd0813eb64aee7766af
Md Rizwan Ansari <85256728+mdrizwan7739@users.noreply.github.com>
2023-02-05 19:43:49 +0530

    Update Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link ,useNavigate} from "react-router-dom";
+import Blog from './Blog';
+import Home from './Home';
 
 const Info = () => {
-
+    const navigate=useNavigate();
+ const handleBlog=()=>{
+       navigate("/Blog");
+    }
+  const handleHome=()=>{
+       navigate("/Home");
+    }
 
     return (
         <div id="info-page">
@@ -11,8 +19,8 @@ const Info = () => {
             <h1> Info page </h1>
             <h2>I am born in India</h2>
             <h2>and I like to play Cricket </h2>
-             <button id="info-to-blog"> Go to Blog</button>
-            <button id="info-to-home"> Go to Home</button>
+             <button id="info-to-blog"  onClick={handleBlog} > Go to Blog</button>
+            <button id="info-to-home"  onClick={handleHome} > Go to Home</button>
         </div>
     )
 }
